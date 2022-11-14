58911446d2ce0109c973a2e83b54043e8bc65cbd
justindou <douerdun@163.com>
2022-11-14 13:09:58 +0800

    fix bundleid

--- autoupdateplistbyname.sh
+++ autoupdateplistbyname.sh
@@ -20,6 +20,11 @@ if [[ -z ${TITLE_NAME} ]]; then
     echo Fallback TITLE_NAME to "Out-Mobile Legends: Bang Bang"
     TITLE_NAME="Out-Mobile Legends: Bang Bang"
 fi
+BUNDLE_ID=$5
+if [[ -z ${BUNDLE_ID} ]]; then
+    echo Fallback BUNDLE_ID to "com.mobile.legends.inhouse2017"
+    BUNDLE_ID="com.mobile.legends.inhouse2017"
+fi
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\
 <!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n\
@@ -34,7 +39,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\
                     <key>kind</key>\n\
                     <string>software-package</string>\n\
                     <key>url</key>" > ${PLIST_NAME}
-echo "                    <string>http://192.168.40.210:9880/mobagame/ipa/mobaiosinhouse_out_${FILE_NAME}.ipa</string>" >> ${PLIST_NAME}
+echo "                    <string>https://intestres.mobilelegends.com/mobagame/ipa/mobaiosinhouse_out_${FILE_NAME}.ipa</string>" >> ${PLIST_NAME}
 echo "            </dict>\n\
                 <dict>\n\
                     <key>kind</key>\n\
@@ -42,13 +47,13 @@ echo "            </dict>\n\
                     <key>needs-shine</key>\n\
                     <true/>\n\
                     <key>url</key>\n\
-                    <string>http://192.168.40.210:9880/mobagame/ipa/res/180.png</string>\n\
+                    <string>https://intestres.mobilelegends.com/mobagame/ipa/res/180.png</string>\n\
                 </dict>\n\
             </array>\n\
             <key>metadata</key>\n\
             <dict>\n\
                 <key>bundle-identifier</key>\n\
-                <string>com.mobile.legends.inhouse</string>\n\
+                <string>${BUNDLE_ID}</string>\n\
                 <key>bundle-version</key>" >> ${PLIST_NAME}
 echo "                <string>${IPA_VERSION}</string>" >> ${PLIST_NAME}
 echo "                <key>kind</key>\n\
